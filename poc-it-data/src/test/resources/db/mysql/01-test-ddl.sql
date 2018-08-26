2ec38ee8a292e2afe3310ce9a0880560fb1b2b44
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2018-08-26 15:17:18 +0200

    FIX - Fixed mysql scripts

--- poc-it-data/src/test/resources/db/mysql/01-test-ddl.sql
+++ poc-it-data/src/test/resources/db/mysql/01-test-ddl.sql
@@ -2,9 +2,7 @@
 -- DDL script
 -- =====================================================================================================================
 
-DROP TABLE IF EXISTS dummytable;
-
-CREATE TABLE dummytable (
+CREATE TABLE IF NOT EXISTS dummytable (
   id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   value VARCHAR(200) NOT NULL
 ) ENGINE=InnoDB;
